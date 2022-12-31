66481544cd0e2d6490453f2fbe22f9bd343aa5c1
Devdeep Ghosh <devarmforce01@gmail.com>
2023-01-01 00:38:48 +0530

    tailwind configured with nodemon

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-var routes = require("./routes");
+const path = require("path");
+const router = require("./routes");
 
-app.use(express.static("public"));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-/* -----Routes----- */
-app.use(routes);
+/* Import all routes from ./routes/index.js */
+app.use(router);
 
 module.exports = app;
