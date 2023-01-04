d2f8c76b195f588d995dddc111509c978ac540d2
Devdeep Ghosh <devarmforce01@gmail.com>
2023-01-04 11:28:45 +0530

    prodution command set

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const csurf = require("tiny-csrf");
+const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const router = require("./routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "any secret string",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    },
+  })
+);
+app.use(cookieParser("any secret string"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cookieParser("any secret string"));
 app.use(csurf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
 
 // Middleware to remove the trailing "/" from a route
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
 /* ----Server---- */
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+app.listen(3030, () => {
+  console.log("Server is listening on port 3030");
 });
