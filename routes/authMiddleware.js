027ca8e67b2e023ad1f6efc22a9f1da5f4fe8598
Devdeep Ghosh <devarmforce01@gmail.com>
2023-01-11 08:48:17 +0530

    added try catch to isVoter middleware

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -12,27 +12,32 @@ const isAdmin = (request, response, next) => {
 };
 
 const isVoter = async (request, response, next) => {
-  const election = await Election.findByPk(request.params.id);
-  if (election.electionStatus == true && election.electionEnded == true) {
-    next();
-  } else {
-    if (request.isAuthenticated() && request.user instanceof Voters) {
-      const voter = await Voters.findVoter(request.user.id);
-      if (voter.eligible_electionId != request.params.id) {
-        request.flash("error", "You are not eligible for this election");
+  try {
+    const election = await Election.findByPk(request.params.id);
+    if (election.electionStatus == true && election.electionEnded == true) {
+      next();
+    } else {
+      if (request.isAuthenticated() && request.user instanceof Voters) {
+        const voter = await Voters.findVoter(request.user.id);
+        if (voter.eligible_electionId != request.params.id) {
+          request.flash("error", "You are not eligible for this election");
+          response.render("voterLogin", {
+            csrfToken: request.csrfToken(),
+            title: "Voter Login",
+            electionId: request.params.id,
+          });
+        } else next();
+      } else {
         response.render("voterLogin", {
           csrfToken: request.csrfToken(),
           title: "Voter Login",
           electionId: request.params.id,
         });
-      } else next();
-    } else {
-      response.render("voterLogin", {
-        csrfToken: request.csrfToken(),
-        title: "Voter Login",
-        electionId: request.params.id,
-      });
+      }
     }
+  } catch (error) {
+    request.flash("error", error.message);
+    response.redirect("back");
   }
 };
 
